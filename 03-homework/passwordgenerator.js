910a7e22e36e6400f627de79487427d53bff50ef
Corbin Herrera <corbin.h.mt@gmail.com>
2019-10-24 01:25:01 -0500

    finished homework

--- 03-homework/passwordgenerator.js
+++ 03-homework/passwordgenerator.js
@@ -1,23 +1,48 @@
 //Generate Password
-
-function generate() {
+function generate(){
 
     //set password length/complexity
-    let complexity = document.getElementById("slider").values;
+    let complexity = document.getElementById("slider").value;
 
     //possible values
-    const values = "ABCDEFGHIJKLMNSOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+"
+    const values = "ABCDEFGHIJKLMNSOQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
     let password = "";
     
     //for loop for password
-    for(var i = 0; i <= complexity; i++) {
-        password = password + values.charAt(Math.floor(Math.random()* Math.floor(values.length - 1)));
-
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
 
     //add password to display
     document.getElementById("display").value = password;
 
+    //keeping track of old passwords
+    document.getElementById("lastpass").innerHTML += password + "<br />";
 
 }
+
+//default length
+document.getElementById("length").innerHTML = "Length: ";
+
+//setting password length based on slider position
+document.getElementById("slider").oninput = function(){
+
+    if(document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "Length: " + + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: ";
+    }
+}
+
+//copy to clipboard
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("Saved to Clipboard!");
+}
+
